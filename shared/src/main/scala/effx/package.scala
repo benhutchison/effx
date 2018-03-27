fdf24b401f74d48a2213db33e19f1e81c1703682
Ben Hutchison <brhutchison@gmail.com>
2018-03-27 12:49:04 +1100

    errorUnless combinator

--- shared/src/main/scala/effx/package.scala
+++ shared/src/main/scala/effx/package.scala
@@ -10,4 +10,22 @@ package object effx {
 
   type Log[R] = Writer[LogMsg, ?] |= R
 
+  type WithErr[E, R] = Fx.prepend[Either[E, ?], R]
+
+  /** the `in` combinator creates a context with an effect stack R, from which operations may be performed.*/
+  def inStack[R] = new InStack[R]
+  class InStack[R] {
+
+    type Rwith[E] = Fx.prepend[Either[E, ?], R]
+
+
+    /** get an effectful `value` of type T. Run predicate `pred` over it. If its not satisfied, fail the stack with an
+    Either[E, ?] error effect, deriving the error value with `error` */
+    def errorUnless[T, E](value: Eff[R, T])(pred: T => Boolean, error: T => E): Eff[Rwith[E], Unit] = for {
+      v <- value.into[Rwith[E]]
+      _ <- if (pred(v)) ().pureEff[Rwith[E]]
+      else left[Rwith[E], E, Unit](error(v))
+    } yield ()
+  }
+
 }
